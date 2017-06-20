55ea1f0e09df0258ff8b4db1df85d8f486c02296
rasmus <rasmus@larry>
2017-06-20 23:56:21 +0200

    First program
    The password program

---
+++ losenord.py
@@ -0,0 +1,16 @@
+##
+## Lindas första program
+##
+
+
+password = "linda9005"
+gissning = ""
+
+while not password==gissning:
+    if gissning == "":
+        print("Skriv in rätt lösenord!")
+    else:
+        print("Fel din dumbom")
+    gissning = input("Lösenord: ")
+print("Bra Jobbat! Du är B!")
+
